82a6391fef22a1472ed8119ece7384d64f9e931e
Enrico Borba <enricozb@gmail.com>
2021-07-03 22:24:36 -1000

    command

--- cmd/clip/main.go
+++ cmd/clip/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/mattn/go-isatty"
+
+	"github.com/enricozb/clip/lib/config"
+)
+
+func getClipboardContent() []byte {
+	if !isatty.IsTerminal(os.Stdin.Fd()) {
+		content, _ := io.ReadAll(os.Stdin)
+		return content
+	}
+
+	for _, clipboard := range config.Clipboards {
+		if content, err := clipboard.Get(); err == nil {
+			return content
+		}
+	}
+
+	return nil
+}
+
+func syncClipboardContent(content []byte) {
+	// update clipboards
+	for _, clipboard := range config.Clipboards {
+		clipboard.Set(content)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	content := getClipboardContent()
+
+	if _, err := os.Stdout.Write(content); err != nil {
+		panic(fmt.Errorf("stdout write: %v", err))
+	}
+	if err := os.Stdout.Close(); err != nil {
+		panic(fmt.Errorf("stdout close: %v", err))
+	}
+
+	syncClipboardContent(content)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/enricozb/clip
 
 go 1.16
+
+require github.com/mattn/go-isatty v0.0.13 // indirect
