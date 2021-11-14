f61430edc49a9a814c3ae72eb144b142dd89a469
Enrico Z. Borba <enricozb@gmail.com>
2021-11-13 22:33:06 -1000

    remove default values

--- default.nix
+++ default.nix
@@ -1,18 +1,15 @@
-{ pkgs ? import <nixpkgs> { }
-, lib ? pkgs.lib
-, buildGoModule ? pkgs.buildGoModule
-}:
+{ pkgs }:
 
-buildGoModule rec {
+pkgs.buildGoModule rec {
   pname = "clip";
   version = "0.0.1";
 
-  src = lib.cleanSource ./.;
+  src = pkgs.lib.cleanSource ./.;
   vendorSha256 = "1y8flix4wd24f4qh56vlfizgj40vpzsrxfaq06cr3bh9d74waccr";
 
   subPackages = [ "cmd/clip" ];
 
-  meta = with lib; {
+  meta = with pkgs.lib; {
     description = "Clip is a synchronizing extensible clipboard";
     homepage = "https://github.com/enricozb/clip";
     license = licenses.unlicense;
